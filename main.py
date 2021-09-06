8127199aa6928441739158eceb71b2468650c7c8
tobiassteggemann <tobias.steggemann@t-online.de>
2021-09-06 16:55:01 +0200

    refactoring added rename function added check function

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import os
 
-workdir = '/Users/tobiassteggemann/Documents/thyssenkrupp/Upload Onepager'
+source_dir = '/Users/tobiassteggemann/Documents/thyssenkrupp/Upload Onepager'
+destination_dir = '/Users/tobiassteggemann/Documents/thyssenkrupp/Upload Onepager/only_first_page'
 
-if not os.path.exists(workdir + '/only_first_page'):
-    os.makedirs(workdir + '/only_first_page')
+if not os.path.exists(destination_dir):
+    os.makedirs(destination_dir)
 
 
 def delete_page(page):
-    for filename in os.listdir(workdir):
+    for filename in os.listdir(source_dir):
         if filename.endswith('.pdf'):
             print(filename)
-            infile = PdfFileReader(os.path.join(workdir, filename))
+            infile = PdfFileReader(os.path.join(source_dir, filename))
             output = PdfFileWriter()
             page_to_keep = infile.getPage(page)
             output.addPage(page_to_keep)
 
-            with open(workdir + '/only_first_page/' + filename, 'wb') as file:
+            with open(os.path.join(destination_dir, filename), 'wb') as file:
                 output.write(file)
+
+
+def rename_files():
+    for filename in os.listdir(destination_dir):
+        if filename.endswith('.pdf'):
+            new_filename = filename.replace('One-Pager ', '')
+            os.rename(os.path.join(destination_dir, filename), os.path.join(destination_dir, new_filename))
+
+
+def check_if_complete():
+    source_ids = [file.split(" ")[0] for file in os.listdir(source_dir) if file.split(" ")[0].isdigit()]
+    destination_ids = [file.split(" ")[0] for file in os.listdir(destination_dir) if file.split(" ")[0].isdigit()]
+    difference = set(source_ids).difference(destination_ids)
+
+    if len(difference) > 0:
+        print("These files were not edited and copied:")
+        print(difference)
+    else:
+        print("All files copied successfully")
 
 
 if __name__ == '__main__':
     delete_page(0)
+    rename_files()
+    check_if_complete()
